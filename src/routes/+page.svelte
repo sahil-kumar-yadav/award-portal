2d2fb9631f616db4af295eec29a55a37d180851f
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-10-09 18:10:47 +0530

    added images and db schema

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
+<svelte:head>
+    <meta http-equiv="refresh" content="0; url=/login" />
+    <title>Award Portal</title>
+</svelte:head>
+
 <h1>Welcome to Award Portal</h1>
 
 Quick Links:
 
 <a href="/login">Login</a>
 <a href="/signup">Signup</a>
-<a href="/forgot>Forgot Password</a>
+<a href="/forgot">Forgot Password</a>
 <a href="/reset">Reset Password</a>
 <br>
 <a href="/admin">Admin</a>
