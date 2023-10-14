08d2604195d0433e91e6fd0d2ce58a83e7bf2b14
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-10-14 15:31:52 +0530

    adjustments to aryan's commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,41 @@
 <svelte:head>
-    <!-- <meta http-equiv="refresh" content="0; url=/login" /> -->
-    <title>Award Portal</title>
+  <!-- <meta http-equiv="refresh" content="0; url=/login" /> -->
+  <title>Award Portal</title>
 </svelte:head>
 
-<h1>Welcome to Award Portal</h1>
+<div class="h1">
+  <h1>Welcome to Award Portal</h1>
+</div>
+<div>
+  <p>Available Pages:</p>
+  <a href="/login">Login</a>
+  <a href="/signup">Signup</a>
+  <a href="/awardcommittee/create">Create Award Committee</a>
+  <a href="/submission/create">Submission Form</a>
 
-Available Pages: 
-<a href="/login">Login</a>
-<a href="/signup">Signup</a>
-<a href="/awardcommittee/create">Create Award Committee</a>
-<a href="/submission/create">Submission Form</a>
+  <p>Quick Links:</p>
 
-Quick Links:
+  <a href="/login">Login</a>
+  <a href="/signup">Signup</a>
+  <a href="/forgot">Forgot Password</a>
+  <a href="/reset">Reset Password</a>
+  <br />
+  <a href="/admin">Admin</a>
+  <a href="/admin/newCommittee">New Committee</a>
+  <br />
+  <a href="/awardcommittee/newAward">New Award</a>
+  <a href="/awardcommittee">Award List</a>
+  <a href="/awardcommittee/1">Award Details</a>
+  <br />
+</div>
 
-<a href="/login">Login</a>
-<a href="/signup">Signup</a>
-<a href="/forgot">Forgot Password</a>
-<a href="/reset">Reset Password</a>
-<br>
-<a href="/admin">Admin</a>
-<a href="/admin/newCommittee">New Committee</a>
-<br>
-<a href="/awardcommittee/newAward">New Award</a>
-<a href="/awardcommittee">Award List</a>
-<a href="/awardcommittee/1">Award Details</a>
-<br>
+<style>
+  div {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: fit-content;
+    margin-inline : auto;
+  }
+</style>
